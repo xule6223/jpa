6413e9d8a1488b60d5673ceb2ab22075d76f4502
lyy-java <2230345161@qq.com>
2019-08-27 21:11:44 +0800

    Initial commit

---
+++ src/main/java/com/offcn/jpa/bean/Person.java
@@ -0,0 +1,53 @@
+package com.offcn.jpa.bean;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Person {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name = "name", nullable = true, length = 20)
+    private String name;
+
+    @Column(name = "age", nullable = true, length = 4)
+    private int age;
+    @Column(name = "password", nullable = true, length = 20)
+    private String password;
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
